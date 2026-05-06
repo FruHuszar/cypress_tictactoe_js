40b9fbf0d42f55478510d276b62198adfe0eb14d
Huszár Fruzsina Anna (SZF_N_2025_2027) <huszar.fruzsina.anna2@diak.szamalk-szalezi.hu>
2026-05-06 14:53:01 +0200

    7-10 feladat

--- cypress/e2e/tictactoe.cy.js
+++ cypress/e2e/tictactoe.cy.js
@@ -48,11 +48,10 @@ describe("template spec", () => {
     cy.get(".jatekter .elem").eq(0).should("not.include.text", "O");
   });
 
-  it("X győzelme megtörténik-e?", function () {
+  it("X győzelme megtörténik-e vízszintesen?", function () {
     cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
 
     cy.get(".jatekter .elem").eq(0).click();
-    cy.get(".jatekter .elem").eq(0).should("include.text", "X");
     cy.get(".jatekter .elem").eq(3).click();
     cy.get(".jatekter .elem").eq(1).click();
     cy.get(".jatekter .elem").eq(4).click();
@@ -63,10 +62,118 @@ describe("template spec", () => {
     });
   });
 
-  // 7. feladat:
-  // 8. feladat:
-  // 9. feladat:
-  // 10. feladat:
+  it("X győzelme megtörténik-e függőlegesen?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(3).click();
+    cy.get(".jatekter .elem").eq(2).click();
+    cy.get(".jatekter .elem").eq(6).click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A győztes: X");
+    });
+  });
+
+  it("X győzelme megtörténik-e átlósan?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(4).click();
+    cy.get(".jatekter .elem").eq(2).click();
+    cy.get(".jatekter .elem").eq(8).click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A győztes: X");
+    });
+  });
+
+  it("O győzelme megtörténik-e vízszintesen?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(8).click();
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(7).click();
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(5).click();
+    cy.get(".jatekter .elem").eq(2).click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A győztes: O");
+    });
+  });
+
+  it("O győzelme megtörténik-e függőlegesen?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(2).click();
+    cy.get(".jatekter .elem").eq(3).click();
+    cy.get(".jatekter .elem").eq(4).click();
+    cy.get(".jatekter .elem").eq(6).click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A győztes: O");
+    });
+  });
+
+  it("O győzelme megtörténik-e átlósan?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(2).click();
+    cy.get(".jatekter .elem").eq(4).click();
+    cy.get(".jatekter .elem").eq(6).click();
+    cy.get(".jatekter .elem").eq(8).click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A győztes: O");
+    });
+  });
+
+  it("Döntetlen kezelve van?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+    cy.get("div:nth-child(1)").click();
+    cy.get("div:nth-child(2)").click();
+    cy.get("div:nth-child(3)").click();
+    cy.get("div:nth-child(5)").click();
+    cy.get("div:nth-child(4)").click();
+    cy.get("div:nth-child(7)").click();
+    cy.get("div:nth-child(6)").click();
+    cy.get("div:nth-child(9)").click();
+    cy.get("div:nth-child(8)").click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("A játék döntetlen!");
+    });
+  });
+
+  it("Leokézás után új játék indul?", function () {
+    cy.visit("https://hrvthlevi.github.io/tictactoeJS--260326/");
+
+    cy.get(".jatekter .elem").eq(0).click();
+    cy.get(".jatekter .elem").eq(3).click();
+    cy.get(".jatekter .elem").eq(1).click();
+    cy.get(".jatekter .elem").eq(4).click();
+    cy.get(".jatekter .elem").eq(2).click();
+
+    //kattintás a windows alertre, majd üresek a mezők utána
+
+    for (let index = 0; index < 9; index++) {
+      cy.get(".jatekter .elem").eq(index).should("not.include.text", "X");
+      cy.get(".jatekter .elem").eq(index).should("not.include.text", "O");
+    }
+  });
+
+  // 10. feladat: Új játék
+  // párosra mindig x / ellentétje
+  // 11. feladat: Játék leállása győzelem után
+
+  // +++ Átló, vízszintes, függőleges győzelmek ell másik irányból
 
   //
   // NEM MEGFELELŐ TESZTEK
